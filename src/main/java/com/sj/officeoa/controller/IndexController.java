87095b0329263ef88075894ced836f431b089c7e
shenjian <634161766@qq.com>
2020-12-05 07:53:59 +0800

    OA项目第一次创建

---
+++ src/main/java/com/sj/officeoa/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.sj.officeoa.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class IndexController {
+
+
+    @GetMapping("/")
+    public  String sayHello(){
+
+        return  "Hello Spring";
+    }
+}
